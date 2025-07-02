365d7a7b4d5de0bc793932d6caf3d5bfdcc75a0f
KAKADIYA VAIRAG <vairag.synfinity@gmail.com>
2025-07-02 18:24:32 +0530

    Adding Styles to React Elements

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,58 +4,6 @@ import './App.css';
 
 
 
-// function App() {
-//   const fname = 'Vairag';
-//   const lname = 'Kakdiya';
-//   const age = '22';
-
-//   const fullName = (fname, lname) => {
-//     return `${lname} ${fname}`
-//   }
-
-
-//   //  const fullName =(fname,lname) =>
-//   //    `${lname} ${fname}`
-
-
-//   const inputValue = <input placeholder='Enter Value' />
-
-
-//   const arry = [1, 2, 3, 4, 5]
-
-//   const obj={
-//     fname:'Vairag',
-//     lname:'kakadiya',
-//     age:'21',
-//   }
-
-
-
-//   return (
-//     <div className="App">
-//       <h1>Hello </h1>
-//       <h1>First Name: {fname}</h1>
-//       <h1>Last Name: {lname} </h1>
-//       <h1>Age: {obj.age} </h1>
-
-
-
-//       <h2>FullName: {fullName(fname, lname)} </h2>
-
-
-
-//       {/* <input placeholder='Enter Value'/> */}
-
-//       {inputValue}
-
-//      <h1> {arry}</h1>
-//      <h1> {arry[0]}</h1>
-
-//      <h1>{obj.fname}</h1>
-
-//     </div>
-//   );
-// }
 
 
 
@@ -65,34 +13,54 @@ function App() {
     description: "In React, a div element functions similarly to a standard HTML div,serving as a generic container for other elements."
   }
 
+  const cssStyle =
+  {
+    margin: '13px',
+    border: '1px solid black',
+    padding: '16px',
+    boxShadow: '0 2px 8px #000'
+
+  }
+
+
   return (
     <div className="App">
       <h1>Learn React</h1>
 
-      <div>
+      <div style={                     //adding inline style//
+        {
+          margin: '13px',
+          border: '1px solid black',
+          padding: '16px',
+          boxShadow: '0 2px 8px #000'
+
+        }
+      }>
+
         <h1> {obj.title}</h1>
+
         <p>{obj.description} </p>
       </div>
 
+      <div style={cssStyle}>           {/* variable style */}
+
+        <h1> {obj.title}</h1>
+
+        <p>{obj.description} </p>
+      </div>
+
+      <div className='divStyle'>        {/* external style */}
+
+        <h1> {obj.title}</h1>
+
+        <p>{obj.description} </p>
+      </div>
+
+
+
+
+
     </div>
-
-
-    // JSX Behind the Scenes 
-
-    // React.createElement("div", {
-    //   className: "App"
-    // }, 
-    //   React.createElement("div", null,
-    //     React.createElement("h3", null, obj.title),
-    //     React.createElement("p", null, obj.description)
-    //   )
-
-    // )
-
-
-
-
-
 
 
   )
