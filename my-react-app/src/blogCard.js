8549edc99e164b34333833215d70ca941dd2ec84
KAKADIYA VAIRAG <vairag.synfinity@gmail.com>
2025-07-03 15:21:17 +0530

    assing Props to Class-based Components(add like Count butten)

--- my-react-app/src/blogCard.js
+++ my-react-app/src/blogCard.js
@@ -1,22 +1,33 @@
 
 import {Clogs} from './utils'
 
-
+import  { Component } from 'react'
 import cssclass from './blogCard.module.css'
 
 
-const blogCard = (properties) => {
+class blogCard extends Component {
 
-    Clogs('hello')
+    state ={
+        likeCount:0
+    }
+
+    onLikeBtnClick = () => {
+        this.setState((prevState) => {
+            return { likeCount: prevState.likeCount + 1 }
+        })
+        Clogs('Like button clicked')
+    }
+    // Clogs('hello')
+  render() {
     return (
-        <div className={cssclass.divStyle}>
-
-            <h1> {properties.title}</h1>
-
-            <p>{properties.description}</p>
-
-        </div>
-    )
+      <div className={cssclass.divStyle}>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+        <p>Like Count: {this.state.likeCount}</p>
+        <button className={cssclass.btnStyle} onClick={this.onLikeBtnClick}>Like</button>
+      </div>
+    );
+  }
 }
 
 export default blogCard;
